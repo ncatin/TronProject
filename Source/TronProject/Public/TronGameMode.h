d4a7ef8f01759d3e7307d7d748d4cf64b5eee5c3
ncatin <34193416+ncatin@users.noreply.github.com>
2025-12-20 19:14:44 -0500

    Created Player Controller and input logic, started on player pawn and gamemode

---
+++ Source/TronProject/Public/TronGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TronGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRONPROJECT_API ATronGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
